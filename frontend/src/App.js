c059d56fe524db21640f24a8d67d9856f353bb57
Abdul Owhab <owhab.cse@gmail.com>
2024-03-28 21:35:14 +0600

    added basic router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,46 @@
 import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "single",
+    element: <Single />,
+  },
+  {
+    path: "write",
+    element: <Write />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Dev Blog</h1>
+      <RouterProvider router={router} />
+      <h2>Dev Blog Footer</h2>
     </div>
   );
 }
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>This is Home page</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="login">Login</Link>
+        </li>
+        <li>
+          <Link to="register">Register</Link>
+        </li>
+        <li>
+          <Link to="single">Signle Blog</Link>
+        </li>
+        <li>
+          <Link to="write">Write</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
