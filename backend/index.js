8c94ab941664560544fb24722ed88ba5c4e671bf
Abdul Owhab <owhab.cse@gmail.com>
2024-03-28 20:46:38 +0600

    added basic setup for react app

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,7 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const app = express();
-const db_connection_url = process.env;
-console.log("URL", db_connection_url);
-mongoose
-  .connect(db_connection_url)
-  .then(() => console.log("Connection to MongoDB Connection Successful.."))
-  .catch((error) => console.error(error));
 
 app.listen("5000", () => {
   console.log("App is listening on port 5000");
