c25210b8a3b2029b340e86af7ba667b84fa42eaa
Abdul Owhab <owhab.cse@gmail.com>
2024-04-03 23:48:49 +0600

    working on blog details page

--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import SingleBlogImage from "./../assets/images/blog.avif";
+import UserProfile from "./../assets/images/profile.webp";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="container">
+        <div className="post-details">
+          <div className="content">
+            <img src={SingleBlogImage} alt="News Details" />
+            <div className="user">
+              <img src={UserProfile} height={50} width={50} alt="" />
+              <div className="info">
+                <span>John Doe</span>
+                <p>Posted 2 hours ago</p>
+              </div>
+            </div>
+          </div>
+          <div className="menu">This is right side</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
